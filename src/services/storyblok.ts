d2aa44e8b0fef458e17cd76f97c1c2fa93a5c63f
Prashant Singh <prashant.singh@elasticpath.com>
2024-05-04 01:40:31 +0100

    add storyblok as preview

--- src/services/storyblok.ts
+++ src/services/storyblok.ts
@@ -1,24 +1,26 @@
+const version = "preview"
+
 export const getTopNavigationContent = async (locale?: string, catalog?: string, apiKey?: string) => {
-    const headerResponse = await fetch(`https://api-us.storyblok.com/v2/cdn/stories/navigation/${catalog}/menu?token=${apiKey? apiKey : process.env.NEXT_PUBLIC_STORYBLOK_API_KEY}&version=published&language=${locale}`, { next: { revalidate: 10 } });
+    const headerResponse = await fetch(`https://api-us.storyblok.com/v2/cdn/stories/navigation/${catalog}/menu?token=${apiKey? apiKey : process.env.NEXT_PUBLIC_STORYBLOK_API_KEY}&version=${version}&language=${locale}`, { next: { revalidate: 10 } });
     return await headerResponse.json();
 }
 
 export const getCategoryContent = async (categories: string[], apiKey?: string) => {
-    const headerResponse = await fetch(`https://api-us.storyblok.com/v2/cdn/stories/categories/${categories.join("/")}?token=${apiKey? apiKey : process.env.NEXT_PUBLIC_STORYBLOK_API_KEY}&version=published`, { next: { revalidate: 10 } });
+    const headerResponse = await fetch(`https://api-us.storyblok.com/v2/cdn/stories/categories/${categories.join("/")}?token=${apiKey? apiKey : process.env.NEXT_PUBLIC_STORYBLOK_API_KEY}&version=${version}`, { next: { revalidate: 10 } });
     return await headerResponse.json();
 }
 
 export const getContentByFolder = async (type: string, page: string[], apiKey?: string) => {
-    const headerResponse = await fetch(`https://api-us.storyblok.com/v2/cdn/stories/${type}/${page.join("/")}?token=${apiKey? apiKey : process.env.NEXT_PUBLIC_STORYBLOK_API_KEY}&version=published`, { next: { revalidate: 10 } });
+    const headerResponse = await fetch(`https://api-us.storyblok.com/v2/cdn/stories/${type}/${page.join("/")}?token=${apiKey? apiKey : process.env.NEXT_PUBLIC_STORYBLOK_API_KEY}&version=${version}`, { next: { revalidate: 10 } });
     return await headerResponse.json();
 }
 
 export const getProductContent = async (productId: string, locale?: string, apiKey?: string) => {
-    const headerResponse = await fetch(`https://api-us.storyblok.com/v2/cdn/stories/products/${productId}?token=${apiKey? apiKey : process.env.NEXT_PUBLIC_STORYBLOK_API_KEY}&version=published&language=${locale}`, { next: { revalidate: 10 } });
+    const headerResponse = await fetch(`https://api-us.storyblok.com/v2/cdn/stories/products/${productId}?token=${apiKey? apiKey : process.env.NEXT_PUBLIC_STORYBLOK_API_KEY}&version=${version}&language=${locale}`, { next: { revalidate: 10 } });
     return await headerResponse.json();
 }
 
 export const getStoryblokContent = async (slug: string, apiKey?: string, locale?: string) => {
-    const headerResponse = await fetch(`https://api-us.storyblok.com/v2/cdn/stories/${slug}?token=${apiKey? apiKey : process.env.NEXT_PUBLIC_STORYBLOK_API_KEY}&version=published&language=${locale}`, { next: { revalidate: 10 } });
+    const headerResponse = await fetch(`https://api-us.storyblok.com/v2/cdn/stories/${slug}?token=${apiKey? apiKey : process.env.NEXT_PUBLIC_STORYBLOK_API_KEY}&version=${version}&language=${locale}`, { next: { revalidate: 10 } });
     return await headerResponse.json();
 }
